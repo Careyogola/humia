c4dbe41681b17fdf05428deeac60139ab5d7c2d7
Carey Ogola <ogolacarey254@gmail.com>
2025-03-19 16:49:48 +0300

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,21 @@
 import React from "react";
+import { FaFacebook } from "react-icons/fa";
+import { BiLogoInstagramAlt } from "react-icons/bi";
 
 const Footer = () => {
     return(
-        <footer className="w-full bg-gray-500 flex flex-col p-4 h-auto">
+        <footer className="w-full bg-[#535357] text-[#f1f1f2] flex flex-col p-4 h-auto">
             <div className="bg-inherit flex flex-row justify-evenly mb-4">
                 <div className="flex flex-col">
                     <div className="flex flex-col mb-4">
-                    <h2 className=" text-white uppercase">need help?</h2>
+                    <h2 className="mb-4 uppercase">need help?</h2>
                     <a href="#">Chat with us</a>
                     <a href="#">Help center</a>
                     <a href="#">Contact us</a>
                     </div>
-                    <div className="flex flex-col mb-4">
+                    <div className="flex flex-col font-light not-last:mb-4">
               
-                    <h2 className="text-white uppercase">useful links</h2>
-                   
-                    
+                    <h2 className="mb-4 uppercase">useful links</h2>
                     <a href="#">Track your order</a>
                     <a href="#">Shipping and Delivery</a>
                     <a href="#">Pick-up stations</a>
@@ -31,7 +31,7 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="flex flex-col">
-                    <h2 className=" text-white uppercase">About jumia</h2>
+                    <h2 className="mb-4 uppercase">About jumia</h2>
                     <a href="#">about us</a>
                     <a href="#">about us</a>
                     <a href="#">about us</a>
@@ -44,7 +44,7 @@ const Footer = () => {
                     <a href="#">about us</a>
                 </div>
                 <div className="flex flex-col">
-                    <h2 className=" text-white uppercase">Make money with jumia</h2>
+                    <h2 className="mb-4 uppercase">Make money with jumia</h2>
                     <a href="#">about us</a>
                     <a href="#">about us</a>
                     <a href="#">about us</a>
@@ -52,9 +52,9 @@ const Footer = () => {
                    
                 </div>
                 <div className="flex flex-col">
-                    <h1 className=" text-white uppercase">Jumia international</h1>
+                    <h1 className="mb-4 uppercase">Jumia international</h1>
                     <div className="flex flex-row">
-                        <div className="flex flex-col">
+                        <div className="flex flex-col text-gray-100">
                             <a href="#">Algeria</a>
                             <a href="#">Ivory Coast</a>
                             <a href="#">Eqypt</a>
@@ -72,7 +72,18 @@ const Footer = () => {
 
                 </div>
             </div>
+            <div className="flex flex-row justify-start gap-7">
+                <div className="flex flex-col">
+                    <h2 className="">JOIN US ON</h2>
+                    <div className="flex flex-row gap-4">
+                    <FaFacebook />
+                    <BiLogoInstagramAlt />
 
+                        </div>
+
+                </div>
+
+            </div>
 
         </footer>
     )
